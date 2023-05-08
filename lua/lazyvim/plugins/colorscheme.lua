10301882e91c00c80d6b03d84280888b55bde15a
shebang <waelse@shebang.org>
2023-05-08 20:58:22 +0200

    Update colorscheme.lua
    remove all plugins

--- lua/lazyvim/plugins/colorscheme.lua
+++ lua/lazyvim/plugins/colorscheme.lua
@@ -1,16 +1,3 @@
 return {
 
-  -- tokyonight
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "moon" },
-  },
-
-  -- catppuccin
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-  },
-}
+  }
